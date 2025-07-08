716ff00035f0fc01d9084ba6203bc4af9054128e
duynghia10096 <duynghia10096@gmail.com>
2025-07-08 21:17:31 +0700

    edit file name docker

--- api-gateway/src/main/java/com/DDN/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/DDN/config/SecurityConfig.java
@@ -5,20 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests()  // Thay thế authorizeRequests() để sử dụng DSL mới
-                .requestMatchers("/**").permitAll() // Cho phép truy cập không xác thực
-                .anyRequest().authenticated()  // Cần xác thực cho các yêu cầu còn lại
-            .and()
-            .formLogin().disable();  // Tắt form login mặc định
-
-        return http.build();
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/prometheus").permitAll() // Cho phép truy cập không cần auth
+                .anyRequest().permitAll()
+            )
+            .csrf().disable()
+            .httpBasic(withDefaults())
+            .build();
     }
 }
